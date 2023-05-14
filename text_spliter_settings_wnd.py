05cd29cd5ef5f8aa4496c1986d3c94f16d41f26c
Viktor Larionov <larionov92spb@gmail.com>
2023-05-14 23:24:46 +0300

    полная обратная связь с флагами

--- text_spliter_settings_wnd.py
+++ text_spliter_settings_wnd.py
@@ -8,10 +8,34 @@ class TextSpliterSettingsWindow(QMainWindow, Ui_Form):
         super().__init__()
         self.setupUi(self)
         self.main_wnd = main_wnd
-        self.wordCheckBox.setChecked(True)
-        self.sentenceCheckBox.setChecked(True)
-        self.paragrafCheckBox.setChecked(True)
-        self.areaCheckBox.setChecked(True)
+        self.wordCheckBox.stateChanged.connect(self.word_clicked)
+        self.sentenceCheckBox.stateChanged.connect(self.sentence_clicked)
+        self.paragrafCheckBox.stateChanged.connect(self.paragraf_clicked)
+        self.areaCheckBox.stateChanged.connect(self.area_clicked)
+
+    def word_clicked(self):
+        if self.wordCheckBox.isChecked():
+            print('Добавили разбиение по словам!')
+        else:
+            print('Убрали разбиение по словам!')
+
+    def sentence_clicked(self):
+        if self.sentenceCheckBox.isChecked():
+            print('Добавили разбиение по предложениям!')
+        else:
+            print('Убрали разбиение по предложениям!')
+
+    def paragraf_clicked(self):
+        if self.paragrafCheckBox.isChecked():
+            print('Добавили разбиение по абзацам!')
+        else:
+            print('Убрали разбиение по абзацам!')
+
+    def area_clicked(self):
+        if self.areaCheckBox.isChecked():
+            print('Добавили разбиение по областям!')
+        else:
+            print('Убрали разбиение по областям!')
 
 
 
